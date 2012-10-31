e8e30ca0236ed73a54f8e9d4ec15c407dda6a414
Adam Ford <adam.ford@greatbigsolutions.com>
2012-10-31 09:37:33 -0600

    Add caching to getRestaurant

--- restaurant.cpp
+++ restaurant.cpp
@@ -1,14 +1,22 @@
 #include <SD.h>
-#include "Restaurant.h"
+#include "restaurant.h"
 
 #define BLOCK_LEN 512
 #define RESTAURANT_START_BLOCK 4000000
 
+uint32_t cachedBlockNumber;
+Restaurant buffer[BLOCK_LEN/sizeof(Restaurant)];
+
 void getRestaurant(int i, Restaurant *r, Sd2Card *card) {
-  Restaurant buffer[BLOCK_LEN/sizeof(Restaurant)];
-  uint32_t block = RESTAURANT_START_BLOCK + i/8;
+  uint32_t blockNumber = RESTAURANT_START_BLOCK + i/8;
+
+  if(blockNumber != cachedBlockNumber) {
+    card->readBlock(blockNumber, (uint8_t *) buffer);
+    cachedBlockNumber = blockNumber;
+  }
+
   uint32_t index = i % 8;
 
-  card->readBlock(block, (uint8_t *) buffer);
   (*r) = buffer[index];
 }
+
