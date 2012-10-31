9542e841aa185584a16b1c3ada89edf7477586be
Adam Ford <adam.ford@greatbigsolutions.com>
2012-10-31 10:25:00 -0600

    Implement sorting

--- assignment2.cpp
+++ assignment2.cpp
@@ -14,11 +14,66 @@
 #define TFT_DC     7  // Data/command line for TFT
 #define TFT_RST    8  // Reset line for TFT (or connect to +5V)
 
+#define RESTAURANTS_COUNT 1024
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 Sd2Card card;
 
 lcd_image_t map_image = { "yeg-sm.lcd", 128, 128 };
+
+void loadDistances(uint16_t horiz, uint16_t vert, RestDist *distances) {
+
+}
+
+void sortDistances(RestDist *distances) {
+  for(int i = 0; i < RESTAURANTS_COUNT - 1; i++) {
+    int16_t smallesti = i;
+    for(int j = i + 1; j < RESTAURANTS_COUNT; j++) {
+      if(distances[j].dist < distances[smallesti].dist) {
+        smallesti = j;
+      }
+    }
+
+    RestDist original = distances[i];
+    distances[i] = distances[smallesti];
+    distances[smallesti] = original;
+  }
+}
+
+void printArray(uint16_t *A, uint16_t n) {
+  for(int i = 0; i < n; i++) {
+    Serial.print(A[i]);
+    Serial.print(", ");
+  }
+  Serial.println("");
+}
+
+void sortDistances(uint16_t *A, uint16_t n) {
+  for(int i = 0; i < n - 1; i++) {
+    int16_t smallesti = i;
+    for(int j = i + 1; j < n; j++) {
+      if(A[j] < A[smallesti]) {
+        smallesti = j;
+      }
+    }
+
+    uint16_t original = A[i];
+    A[i] = A[smallesti];
+    A[smallesti] = original;
+
+    Serial.print("Iteration #");
+    Serial.print(i);
+    Serial.print(": ");
+    printArray(A, n);
+  }
+}
+
+
+void displayClosestRestaurants(uint16_t horiz, uint16_t vert) {
+  RestDist distances[RESTAURANTS_COUNT];
+  loadDistances(horiz, vert, distances);
+  sortDistances(distances);
+}
 
 void setup(void) {
     Serial.begin(9600);
@@ -46,13 +101,24 @@ void setup(void) {
     Restaurant r;
     getRestaurant(0, &r, &card);
     Serial.println(r.name);
+    
+    uint16_t A[10] = { 16, 50, 40, 3, 11, 20, 2, 14, 8, 12 };
+    uint16_t n = 10;
+
+    Serial.print("Original array: ");
+    printArray(A, n);
+    sortDistances(A, 10);
 }
 
 void loop() {
   uint16_t horiz = getHorizontal();
   uint16_t vert = getVertical();
+  uint8_t buttonIsPressed = isButtonPressed();
 
   tft.drawPixel(horiz, vert, 0);
   lcd_image_draw(&map_image, &tft, horiz, vert, horiz, vert, 1, 1);
+  if(buttonIsPressed) {
+    displayClosestRestaurants(horiz, vert);
+  }
 }
 
